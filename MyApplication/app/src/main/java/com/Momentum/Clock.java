6a2af1a90015cc62ccf0f406ca78b647692cdda5
Yop Spanjers <yopspanjers@gmail.com>
2015-10-14 19:35:56 +0200

    Fixed the fucker

--- MyApplication/app/src/main/java/com/Momentum/Clock.java
+++ MyApplication/app/src/main/java/com/Momentum/Clock.java
@@ -1,8 +1,12 @@
 package com.test.Momentum.Momentum;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 
 import java.util.Calendar;
 
@@ -18,7 +22,7 @@ public abstract class Clock {
             Color.rgb(11,8,112),
             Color.rgb(244,221,146),
             Color.rgb(18,123,108),
-            Color.rgb(114,0,0), //5
+            Color.rgb(114,0,0),     //5
             Color.rgb(249,177,61),
             Color.rgb(121,170,211),
             Color.rgb(255,194,226),
@@ -28,7 +32,7 @@ public abstract class Clock {
             Color.rgb(216,216,216),
             Color.rgb(243,229,69),
             Color.rgb(52,42,57),
-            Color.rgb(237,61,0), //15
+            Color.rgb(237,61,0),    //15
             Color.rgb(63,158,50),
             Color.rgb(0,76,40),
             Color.rgb(191,0,14),
@@ -43,14 +47,17 @@ public abstract class Clock {
     static Paint innerCircle = new Paint();
     static Paint outerCircle = new Paint();
 
+    static int _circleWidth;
+
     public static void drawOuterCircle(Canvas canvas)
     {
         Calendar calendar = Calendar.getInstance();
+        _circleWidth = (canvas.getWidth()/2 - 20);
         int x = canvas.getWidth()/2;
         int y = canvas.getHeight()/2;
         int intTime = calendar.get((Calendar.HOUR_OF_DAY));
         outerCircle.setColor(colors[intTime]);
-        canvas.drawCircle(x,y,400,outerCircle);
+        canvas.drawCircle(x,y,(x - 40),outerCircle);
     }
 
     public static void drawInnerCircle(Canvas canvas)
@@ -58,7 +65,7 @@ public abstract class Clock {
         Calendar calendar = Calendar.getInstance();
         float minute = calendar.get(Calendar.MINUTE);
         float second = calendar.get(Calendar.SECOND);
-        int time = (int)(minute + (second/60));
+        int time = (int)(minute + (second/59));
         int width = ((400/60)*time);
 
         int x = canvas.getWidth()/2;
@@ -73,28 +80,28 @@ public abstract class Clock {
         int x = canvas.getWidth()/2;
         int y = canvas.getHeight()/2;
         outerCircle.setColor(colors[intTimeHours]);
-        canvas.drawCircle(x,y,400,outerCircle);
+
+        canvas.drawCircle(x,y,(x - 40),outerCircle);
     }
 
     public static void drawInnerCircle(Canvas canvas, int intTimeHours, int intTimeMinutes)
     {
-        Calendar calendar = Calendar.getInstance();
+        //Calendar calendar = Calendar.getInstance();
+        if ( intTimeHours > 23 || intTimeHours < 0 || intTimeMinutes > 59 || intTimeMinutes < 0)
+        {
+            throw new IllegalArgumentException("This is not a valid time");
+        }
+
         float minute = intTimeMinutes;
         float second = 0;
-        int time = (int)(minute + (second/60));
-        int width = ((400/60)*time);
+        int time = (int)(minute + (second/59));
         int x = canvas.getWidth()/2;
         int y  = canvas.getHeight()/2;
-        innerCircle.setColor(colors[intTimeHours+1]);
-        canvas.drawCircle(x,y,width,innerCircle);
-    }
+        int width = (((x-40)/59)*time);
 
-    public static void drawOuterCircleSet(Canvas canvas, int screenWidth, int screenHeight, int hour)
-    {
-        Calendar calendar = Calendar.getInstance();
-        int x = screenWidth/2;
-        int y = screenHeight/2;
-        outerCircle.setColor(colors[hour]);
-        canvas.drawCircle(x,y,400,outerCircle);
+        innerCircle.setColor(colors[intTimeHours+1]);
+        //canvas.drawCircle(x,y,width,innerCircle);
+        canvas.drawCircle(x,y,(((x - 40)/59)*time),innerCircle);
+
     }
 }
